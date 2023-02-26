4a63960d6049431483e53145bfcc5177690d75d4
primayuda <aprita.primayuda@gmail.com>
2023-02-26 19:27:32 +0700

    adding daisy ui carousel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,5 +68,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar"), require("@tailwindcss/typography")],
+  plugins: [require("tailwind-scrollbar"), require("@tailwindcss/typography"), require("daisyui")],
 };
